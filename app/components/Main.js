49d275cdddd1706449fdfb90a321d4326dcd6814
khoatran <khoatran.kt1991@gmail.com>
2017-05-30 20:27:03 +0700

    cm

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,11 +4,7 @@ import Nav from 'Nav';
 import Home from 'Home';
 import Page1 from 'Page1';
 import Login from 'Login';
-<<<<<<< HEAD
 import Account from 'account/Account';
-=======
-import SignIn from 'SignIn';
->>>>>>> try to change
 
 const fakeAuth = {
   isAuthenticated: false,
@@ -28,11 +24,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
       <Component {...props}/>
     ) : (
       <Redirect to={{
-<<<<<<< HEAD
         pathname: '/account',
-=======
-        pathname: '/signIn',
->>>>>>> try to change
         state: { from: props.location }
       }}/>
     )
@@ -45,20 +37,12 @@ class Main extends React.Component {
                     <div>
                         <ul>
                             <li><Link to="/">Home</Link></li>
-<<<<<<< HEAD
                             <li><Link to="/account">Account</Link></li>
-=======
-                            <li><Link to="/signIn">Login</Link></li>
->>>>>>> try to change
                             <li><Link to="/page1">Page1</Link></li>
                         </ul>
                         <Route exact path="/" component={Home}/>
                         <PrivateRoute path="/page1" component={Page1}/>
-<<<<<<< HEAD
                         <Route path="/account" component={Account}/>
-=======
-                        <Route path="/signIn" component={SignIn}/>
->>>>>>> try to change
                     </div>
                 </Router>);
     }
