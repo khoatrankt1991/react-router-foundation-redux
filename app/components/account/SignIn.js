7cffb2bf42e2fbca0a03e6109cdcd7299c03a4e4
khoatran <khoatran.kt1991@gmail.com>
2017-05-31 09:12:34 +0700

    fixed conflit

--- app/components/account/SignIn.js
+++ app/components/account/SignIn.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import {connect} from 'react-redux';
 class SignIn extends React.Component {
     handleSubmit(e) {
@@ -16,23 +15,4 @@ class SignIn extends React.Component {
     }
 }
 module.exports = connect()(SignIn);
-=======
 
-class SignIn extends React.Component {
-    
-    handleSubmit(e) {
-        e.preventDefault();
-        alert(this.refs.username);
-    }
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit.bind(this)}>
-                <input type="text" ref="username"/>
-                <input type="password" ref="password"/>
-                <button type="submit">Sign In</button>
-            </form>
-        );
-    }
-}
-module.exports = SignIn;
->>>>>>> try to change
